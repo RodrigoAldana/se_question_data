6b7ac2e1219e9c52081e24db5bfee8bb76a06494
RodrigoAldana <rodrigo.aldana.lopez@gmail.com>
2023-01-17 00:05:41 +0100

    Changed the way the overall error is computed

--- testbench.m
+++ testbench.m
@@ -10,8 +10,8 @@ theta = randn(n,1);
 w = 5*randn(m,1);  
 x = M*theta + w;
 
-est_theta1 = pinv(M)*x;
+est_theta1 = inv(M'*M)*M'*x;
 est_theta2 = M_special*x;
 
-std1 = std(est_theta1-theta),
-std2 = std(est_theta2-theta),
+std1 = sqrt((est_theta1-theta)'*(est_theta1-theta)/n),
+std2 = sqrt((est_theta2-theta)'*(est_theta2-theta)/n),
